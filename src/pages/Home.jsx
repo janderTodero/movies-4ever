497dce2ce6994b8beeb3030aa70d7f6176d0b9ec
janderTodero <jandertodero@gmail.com>
2025-07-14 15:48:45 -0300

    adding swiper

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,10 @@
 
 import { useEffect, useState } from "react";
 import { getNowPlaying } from "../services/api";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
 
 function Home() {
   const [movies, setMovies] = useState([]);
@@ -13,21 +17,29 @@ function Home() {
   return (
     <div className="p-4">
       <h2 className="text-2xl font-bold mb-4 text-white">Filmes em Cartaz</h2>
-      <div className="flex overflow-x-auto space-x-4 snap-x snap-mandatory scrollbar-hide">
+
+      <Swiper
+        modules={[Navigation, Autoplay]}
+        spaceBetween={16}
+        slidesPerView={5}
+        navigation
+        autoplay={{ delay: 3000, disableOnInteraction: true }}
+        loop={true}
+        className="mySwiper"
+      >
         {movies.map((movie) => (
-          <div
-            key={movie.id}
-            className="flex-shrink-0 w-48 snap-start bg-gray-800 p-2 rounded text-white"
-          >
-            <img
-              src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-              alt={movie.title}
-              className="rounded mb-2"
-            />
-            <h2 className="text-sm font-bold">{movie.title}</h2>
-          </div>
+          <SwiperSlide key={movie.id}>
+            <div className="bg-gray-800 p-2 rounded text-white">
+              <img
+                src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+                alt={movie.title}
+                className="rounded mb-2"
+              />
+              <h2 className="text-sm font-bold">{movie.title}</h2>
+            </div>
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
     </div>
   );
 }
